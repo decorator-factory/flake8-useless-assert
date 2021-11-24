bf780d9a30b4e0a18f16ce699519763301918f25
decorator-factory <42166884+decorator-factory@users.noreply.github.com>
2021-11-24 09:59:54 +0300

    Release 0.2.0

--- flake8_useless_assert/plugin.py
+++ flake8_useless_assert/plugin.py
@@ -9,7 +9,7 @@ from .patch_const import LegacyConstantRewriter
 
 class UselessAssert:
     name = "flake8-useless-assert"
-    version = "0.1.3"
+    version = "0.2.0"
 
     def __init__(self, tree: ast.Module) -> None:
         self._tree = tree
